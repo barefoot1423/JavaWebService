0172430c70b576e270a79bc8bf076aa49ce24fd6
Hyunwoo Cho <barefoot1423@gmail.com>
2019-11-02 19:57:32 -0400

    initial commit

---
+++ jaxbxjc/src/main/java/com/barefoot/jaxb/jaxbxjc/JaxbxjcApplication.java
@@ -0,0 +1,13 @@
+package com.barefoot.jaxb.jaxbxjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxbxjcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaxbxjcApplication.class, args);
+    }
+
+}
---
+++ jaxbxjc/src/test/java/com/barefoot/jaxb/jaxbxjc/JaxbxjcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barefoot.jaxb.jaxbxjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxbxjcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
